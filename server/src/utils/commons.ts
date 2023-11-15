a65d0c2c15619e51cf235ce45ecb9180837b4d47
Rafaela Inácio <rafaelafsinacio@gmail.com>
2023-11-15 19:31:22 +0000

    feat(common): add common functions

---
+++ server/src/utils/commons.ts
@@ -0,0 +1,11 @@
+export function shuffleArray<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export function whatsappTemplate<T>(match: T) {
+  console.log("🚀 ~ file: createTemplate.ts:4 ~ match:", match);
+}
